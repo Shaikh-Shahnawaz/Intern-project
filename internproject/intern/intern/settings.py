824f3928155ecaa04ee2aa07bdbe3f4a5d145982
Shaikh Mohammed Shahnawaz <52959114+Shaikh-Shahnawaz@users.noreply.github.com>
2021-09-07 20:42:49 +0530

    Update settings.py

--- internproject/intern/intern/settings.py
+++ internproject/intern/intern/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dtk)%gx82iy%zt8+jazq8i!az*3wfxewn%zye7=nryz2*&v_5s'
+#SECRET_KEY = 'django-insecure-dtk)%gx82iy%zt8+jazq8i!az*3wfxewn%zye7=nryz2*&v_5s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
